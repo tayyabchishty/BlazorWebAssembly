ee0603e3bf307dd27aa451cbbdb74b1fa215c0af
tayyab hussain <syed_tayyab_hussain@yahoo.com>
2021-09-11 16:54:48 +0500

    asfasfdsf

--- BlazorWebAssembly/Shared/Employee.cs
+++ BlazorWebAssembly/Shared/Employee.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BlazorWebAssembly.Shared.Utilities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,12 @@ namespace BlazorWebAssembly.Shared
     public class Employee
     {
         public int EmployeeId { get; set; }
+
+        [Required]
+        [MinLength(2)]
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [AllowedEmailDomain("gmail.com",ErrorMessage = "Invalid domain")]
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
---
+++ BlazorWebAssembly/Shared/Utilities/AllowedEmailDomainAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssembly.Shared.Utilities
+{
+    public class AllowedEmailDomainAttribute : ValidationAttribute
+    {
+
+        public AllowedEmailDomainAttribute(string allowedDomain)
+        {
+            AllowedDomain = allowedDomain;
+        }
+
+        public string AllowedDomain { get; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || value.ToString().Length == 0)
+            {
+                return new ValidationResult("Email address is required");
+            }
+            else
+            {
+                if (value.ToString().IndexOf('@') == -1)
+                {
+                    return new ValidationResult("Invalid email address");
+                }
+
+                string[] strings = value.ToString().Split('@');
+                if (strings[1].ToLower() == AllowedDomain.ToLower())
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult(this.ErrorMessage ?? $"Email domain must be {AllowedDomain}");
+                }
+            }
+        }
+    }
+}
